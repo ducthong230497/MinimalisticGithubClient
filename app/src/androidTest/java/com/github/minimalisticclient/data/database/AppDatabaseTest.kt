0ff08dafea8bd250be488a0e661e04b536747ee2
Duc Thong <ducthong2304@gmail.com>
2021-11-30 11:17:20 +0700

    add simple test case

---
+++ app/src/androidTest/java/com/github/minimalisticclient/data/database/AppDatabaseTest.kt
@@ -0,0 +1,16 @@
+package com.github.minimalisticclient.data.database
+
+import junit.framework.TestCase
+
+class AppDatabaseTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testUserDao() {}
+
+    fun testRepoDao() {}
+}
